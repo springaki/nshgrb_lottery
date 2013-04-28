8f16ade3bf0b799c1335469b62ebca0b47c58b55
Aki <aki.onetime@gmail.com>
2013-04-28 23:27:36 +0900

    same people case

--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -12,7 +12,7 @@ describe Lottery do
 		end
 	end
 
-	context 'when winners is not empty' do
+	context 'when winners is 1 people' do
 		before do
 			@lottery = Lottery.new(1)
 		end
@@ -21,10 +21,27 @@ describe Lottery do
 			@lottery.winners.should eq []
 		end
 
-		describe 'when add' do
+		describe 'when add 1 people' do
 			it '#winners is 1 people' do
 				@lottery.add('paul', 1)
 				@lottery.winners.should eq ['paul']
+			end
+		end
+	end
+
+	context 'when winners is same people' do
+		before do
+			@lottery = Lottery.new(3)
+			@lottery.add("John", 1)
+			@lottery.add("Tom", 2)
+			@lottery.add("Bill", 5)
+			@lottery.add("Woz", 2)
+			@lottery.add("Ken", 10)
+		end
+
+		describe 'when add 5 people' do
+			it 'John is 1/20 hit' do
+				(@lottery.count("John") / @lottery.size.to_f).should be_within(0.03).of(0.07)
 			end
 		end
 	end

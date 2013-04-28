e4a461cfca5fdbc297ab3d13e9698483cdd9377e
Aki <aki.onetime@gmail.com>
2013-04-28 17:28:31 +0900

    add sprc

--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -1,5 +1,17 @@
 require './lottery'
 
 describe Lottery do
+	before do
+	end
+
+	context 'when winners is empty' do
+		lottery = Lottery.new(0)
+		it '#winners is 0' do
+			lottery.winners.should eq []
+		end
+	end
+
+	context 'when winners is not empty' do
+	end
 end
 
